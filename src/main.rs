4b8524c1050c53f36066192625235e822a68270a
Verboven Koen <koenverboven@gmail.com>
2026-01-08 10:30:29 +0100

    Add function calculate_weight_on_moon

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
 fn main() {
-    println!("Hello, world!, from Rust! Keep coding!");
-    println!("Koen Verboven - 2026 januari 6");
+    let yourname = "Koen Verboven";
+    let earth_weight: f32 = 75.0; // Example weight on Earth in kg
+
+    println!("Koen Verboven 8 januari 2026");
+
+    println!(
+        "Weight on the Earth of {:?}: {:.2} kg",
+        yourname, earth_weight
+    );
+
+    let moon_weight = calculate_weight_on_moon(earth_weight);
+    println!(
+        "Weight on the Moon of {:?}: {:.2} kg",
+        yourname, moon_weight
+    );
+}
+
+fn calculate_weight_on_moon(earth_weight: f32) -> f32 {
+    let moon_gravity_factor: f32 = 0.165;
+    earth_weight * moon_gravity_factor
 }
