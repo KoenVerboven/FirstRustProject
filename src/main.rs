43267a137be9c20181454e2f7af7a2aec321ca74
Verboven Koen <koenverboven@gmail.com>
2026-09-23 12:00:34 +0200

    Refactor variable names to snake_case_formatting

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,34 @@ use std::io;
 
 fn main() {
     println!("Enter your name:");
-    let mut nameinput = String::new();
-    io::stdin().read_line(&mut nameinput).unwrap();
-    let yourname = nameinput.trim();
+    let mut name_input = String::new();
+    io::stdin().read_line(&mut name_input).unwrap();
+    let your_name = name_input.trim();
 
     println!("Enter your weight in kg (example: 75.4):");
-    let mut weightinput = String::new();
-    io::stdin().read_line(&mut weightinput).unwrap();
-    let yourweight: f32 = weightinput.trim().parse().unwrap();
+    let mut weight_input = String::new();
+    io::stdin().read_line(&mut weight_input).unwrap();
+    let your_weight: f32 = weight_input.trim().parse().unwrap();
 
     println!("Enter your length in m (example: 1.8):");
-    let mut lengthinput = String::new();
-    io::stdin().read_line(&mut lengthinput).unwrap();
-    let yourlength: f32 = lengthinput.trim().parse().unwrap();
+    let mut length_input = String::new();
+    io::stdin().read_line(&mut length_input).unwrap();
+    let your_length: f32 = length_input.trim().parse().unwrap();
 
-    let calculatedbmi = calculate_bmi(yourweight, yourlength);
-    println!("BMI of {:?}: {:.2}", yourname, calculatedbmi);
+    let calculated_bmi = calculate_bmi(your_weight, your_length);
+    println!("BMI of {:?}: {:.2}", your_name, calculated_bmi);
 
-    if calculatedbmi < 18.5 {
+    if calculated_bmi < 18.5 {
         println!("Underweight");
-    } else if calculatedbmi >= 18.5 && calculatedbmi < 25.0 {
+    } else if calculated_bmi >= 18.5 && calculated_bmi < 25.0 {
         println!("Normal weight");
-    } else if calculatedbmi >= 25.0 && calculatedbmi < 30.0 {
+    } else if calculated_bmi >= 25.0 && calculated_bmi < 30.0 {
         println!("Overweight");
     } else {
         println!("Obesity");
     }
 }
 
-fn calculate_bmi(yourweight: f32, yourlength: f32) -> f32 {
-    yourweight / (yourlength * yourlength)
+fn calculate_bmi(your_weight: f32, your_length: f32) -> f32 {
+    your_weight / (your_length * your_length)
 }
